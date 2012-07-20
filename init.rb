b77f79088a0bd3e4106ebb9d674a1b3aa2d635f3
Probeerka <probeerka@probeerka.com>
2012-07-21 01:31:00 +0400

    Lesson 26

--- init.rb
+++ init.rb
@@ -1,14 +1,25 @@
+require_relative "item_container"
 require_relative "cart"
 require_relative "item"
+require_relative "real_item"
+require_relative "virtual_item"
+require_relative "order"
 
-item1 = Item.new({:price => 10, :weight => 100, :name => "Car"})
-item2 = Item.new({ :weight => 100, :name => "Car"})
+item1 = VirtualItem.new({:price => 10, :weight => 100, :name => "Car"})
+item2 = RealItem.new({ :price => 30, :weight => 100, :name => "Car"})
+item3 = RealItem.new({ :weight => 100, :name => "Dishwasher"})
+
 cart = Cart.new
 cart.add_item item1
 cart.add_item item2
-cart.delete_invalid_items
+cart.remove_item
+puts cart.items.size
 
-p cart.items
+order = Order.new
+order.add_item item1
+order.add_item item2
+order.add_item item3
+puts order.items.size
 
-
+puts order.count_valid_items
 
--- item.rb
+++ item.rb
@@ -1,16 +1,47 @@
 class Item
+    
+    @@discount = 0.05
+
+    def self.discount
+        if Time.now.month == 7
+            return @@discount += 0.1
+        else
+            return @@discount
+        end
+    end
+    
     def initialize(options = {})
-        @price = options[:price]
+        @real_price = options[:price]
         @weight = options[:weight]
         @name = options[:name]
     end
-    attr_reader :price, :weight, :name
+
+    attr_reader :real_price, :name
     attr_writer :price
 
     def info
         yield(price)
-        yield(weight)
         yield(name)
+    end
+    
+    def price
+        (@real_price - @real_price*self.class.discount) + tax if @real_price
+    end
+
+    private
+
+    def tax
+        type_tax = if self.class == VirtualItem
+        1
+        else
+        2
+        end
+        cost_tax = if @real_price > 5
+         @real_price*0.2
+        else
+         @price*0.1
+        end
+        cost_tax + type_tax
     end
 
 end
